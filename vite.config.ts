3f69e64a6f7054963123cd2ac193b1422609bfa7
isamu <isamu@to-kyo.to>
2025-10-01 16:31:23 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-import { cloudflare } from "@cloudflare/vite-plugin"
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		vueDevTools(),
-		cloudflare(),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./client', import.meta.url))
-		},
-	},
-})
+  plugins: [vue(), vueDevTools(), cloudflare(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./client", import.meta.url)),
+    },
+  },
+});
