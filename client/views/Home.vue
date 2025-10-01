56a1a4085148a250d702cfe37e5e1d613cf38dd0
isamu <isamu@to-kyo.to>
2025-10-02 05:10:32 +0900

    web

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="min-h-screen flex items-center justify-center p-4">
     <div class="max-w-xl w-full space-y-4">
+      <div class="text-pretty text-center text-3xl font-bold sm:text-5xl sm:font-extrabold md:text-6xl text-foreground">
+        MulmoCast
+      </div>
+      <div class="col-span-3 rounded-2xl border border-muted bg-card h-80 md:h-auto p-6 md:p-10 relative overflow-hidden">
+        aa
+      </div>
+      <div class="group rounded-2xl border border-muted bg-card hover:bg-muted/50 p-4 md:p-6 relative">
+        <div class="mt-4 flex items-center gap-2">
+          AA
+        </div>
+        <p class="mt-2 text-muted-foreground group-hover:text-foreground group-focus:text-foreground">
+          aa
+        </p>
+      </div>
       <Card>
         <CardHeader>
-          <CardTitle>MulmoCast</CardTitle>
+          <CardTitle>Download Mac arm</CardTitle>
         </CardHeader>
         <CardContent class="space-y-4">
           <a :href="currentMac?.updateTo?.url">Download {{ currentMac?.version }} ({{ currentMac?.updateTo?.url }})</a>
@@ -73,17 +87,18 @@ const fetchDataMac = async () => {
   currentMac.value = releases.find((release: MacData) => release.version === currentRelease);
 
 };
-/*
+
 const fetchDataWin = async () => {
   const response = await fetch("https://s3.aws.mulmocast.com/releases/prod/win32/x64/RELEASES");
-  const result = await response.json();
-  const { currentRelease, releases } = result;
-  currentWin.value = releases.find(release => release.version === currentRelease);
+  const result = await response.text();
+  console.log(result.split("\n").map(line => line.split(/\s/)));
+  // const { currentRelease, releases } = result;
+  // currentWin.value = releases.find(release => release.version === currentRelease);
 };
-*/
+
 
 fetchDataMac();
-//fetchDataWin();
+fetchDataWin();
 
 function onClear() {
   text.value = ''
