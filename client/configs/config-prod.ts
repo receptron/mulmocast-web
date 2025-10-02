2dc3d7837133979cb8fee5b9697520dd5c760250
isamu <isamu@to-kyo.to>
2025-10-02 13:38:49 +0900

    add config

---
+++ client/configs/config-prod.ts
@@ -0,0 +1,1 @@
+export const macReleaseURL = "https://s3.aws.mulmocast.com/releases/prod/darwin/arm64/RELEASES.json";
---
+++ client/configs/config.ts
@@ -0,0 +1,1 @@
+export const macReleaseURL = "https://s3.aws.mulmocast.com/releases/dev/darwin/arm64/RELEASES.json";
