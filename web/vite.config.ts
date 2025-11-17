112e2ae0ce9d97674db28ed74fb819131f319317
isamu <isamu@to-kyo.to>
2025-11-18 08:36:27 +0900

    mono

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,11 +5,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./client", import.meta.url)),
