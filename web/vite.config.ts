25ed597bb728813b968e3f58c1c2fc63611f2717
isamu <isamu@to-kyo.to>
2025-11-18 08:57:22 +0900

    mono

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./client", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
